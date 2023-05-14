39d21cc86b4a0e817fa294140a0538dbcd476a0e
Nafis-Abdelkarim <kazcrb@gmail.com>
2023-05-14 23:07:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
+
+  const api ={
+    key: "c94ca974d3cd263e08529177cb59bd6a",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    const dateBuilder = (d)=>{
+      console.log("hello world")
+    }
+
+    <div className="app">
+      <main>
+        <div className='search-box'>
+          <input
+          type='text'
+          className='search-bar'
+          placeholder='Search...'/>
+        </div>
+        <div className='loaction-box'>
+          <div className='location'>New York City</div>
+          <div className='date'>{dateBuilder(new Date)}</div>
+        </div>
+      </main>
     </div>
   );
 }
